31582bd11618cd0000bfb553af565da59ab2bc1c
Yassin Ezaouibi <zouibiyassin03@gmail.com>
2024-01-18 21:56:40 +0100

    finally database connect successfully

---
+++ src/test/java/com/yassin/labxpert/services/EchantillonServiceTest.java
@@ -0,0 +1,28 @@
+package com.yassin.labxpert.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EchantillonServiceTest {
+
+    @Test
+    void deleteEchantillonById() {
+    }
+
+    @Test
+    void addEchantillon() {
+    }
+
+    @Test
+    void modifierEchantillon() {
+    }
+
+    @Test
+    void getEchantillonById() {
+    }
+
+    @Test
+    void getEchantillons() {
+    }
+}
